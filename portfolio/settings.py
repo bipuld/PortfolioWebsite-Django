146e5eeec662f432ef6245b7c8da80924b07d434
Bipul Dawadi <bipuldawadi14@gmail.com>
2023-10-05 20:06:02 +0545

    Done

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -92,12 +92,6 @@ DATABASES = {
         'HOST':'Localhost',
         'PORT':'5432'
     }
-# settings.py
-
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
 }
 # DATABASES['default']:dj_database_url.config()
 
